53e5fc466abbc31f672c3306c8a11599ead0e892
Thomas Montalto <thomasmonta98@gmail.com>
2021-11-30 17:49:04 +1100

    done with assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,11 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
-  end
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all
+    end
 
   def show
     @post = Post.find(params[:id])
